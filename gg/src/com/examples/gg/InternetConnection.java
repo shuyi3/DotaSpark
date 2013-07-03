21d1ada479c15ac5d188ba9ff3bfb556cc1009d5
Haoran <Haoran@Haoran-PC>
2013-07-02 23:22:27 -0600

    Added network handling

---
+++ gg/src/com/examples/gg/InternetConnection.java
@@ -0,0 +1,24 @@
+package com.examples.gg;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class InternetConnection {
+    public boolean isOnline(Activity a) {
+        ConnectivityManager cm =
+            (ConnectivityManager) a.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+        return false;
+    }
+    
+    public void networkToast(Activity a){
+    	Toast.makeText(a,"Please check your network connection!", Toast.LENGTH_SHORT).show();
+    }
+    
+}
