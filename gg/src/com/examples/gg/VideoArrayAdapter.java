db6e6dbf9414ec0f3492bb59fdbb790f31ece97a
shuyi3 <bugdx123@gmail.com>
2013-07-05 01:21:08 -0600

    ViewHolder, 快速读取. 改了一下loadmore的pregrassingbar

--- gg/src/com/examples/gg/VideoArrayAdapter.java
+++ gg/src/com/examples/gg/VideoArrayAdapter.java
@@ -3,7 +3,6 @@ package com.examples.gg;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -19,32 +18,36 @@ public class VideoArrayAdapter extends ArrayAdapter<String> {
 	private final Context context;
 	private final ArrayList<String> values;
 	private ArrayList<Video> videos;
+	private LayoutInflater inflater;
  
 	public VideoArrayAdapter(Context context, ArrayList<String> values, ArrayList<Video> videos) {
 		super(context, R.layout.videolist, values);
 		this.context = context;
 		this.values = values;
 		this.videos = videos;
+		inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
  
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		LayoutInflater inflater = (LayoutInflater) context
-			.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		
+		ViewHolder holder;
  
-		View rowView = inflater.inflate(R.layout.videolist, parent, false);
-		TextView titleView = (TextView) rowView.findViewById(R.id.videotitle);
-		ImageView imageView = (ImageView) rowView.findViewById(R.id.thumbnail);
+		if (convertView == null) {
+			convertView = inflater.inflate(R.layout.videolist, parent, false);
+		
+			holder = new ViewHolder();
+		
+			holder.titleView = (TextView) convertView.findViewById(R.id.videotitle);
+			holder.imageView = (ImageView) convertView.findViewById(R.id.thumbnail);
 //		ImageView uploaderView = (ImageView) rowView.findViewById(R.id.uploaderImage);
-		titleView.setText(values.get(position));
 		
 		//set the description
 //		TextView descView = (TextView) rowView.findViewById(R.id.description);
 //		descView.setText(videos.get(position).getVideoDesc());
 		
 		//set the author
-		TextView authorView = (TextView) rowView.findViewById(R.id.videouploader);
-		authorView.setText(videos.get(position).getAuthor());
+			holder.authorView = (TextView) convertView.findViewById(R.id.videouploader);
 		
 		//set the update time
 //		TextView timeView = (TextView) rowView.findViewById(R.id.updatetime);
@@ -52,11 +55,24 @@ public class VideoArrayAdapter extends ArrayAdapter<String> {
  
 		// Change icon based on name
 			
-        new DownloadImage(videos.get(position).getThumbnailUrl()).execute(imageView);
 //        new DownloadImage(videos.get(position).getUploaderThumUrl()).execute(uploaderView);
+			convertView.setTag(holder);
+		}else{
+			holder = (ViewHolder) convertView.getTag();
+		}
+		
+		holder.titleView.setText(values.get(position));
+		holder.authorView.setText(videos.get(position).getAuthor());
+		new DownloadImage(videos.get(position).getThumbnailUrl()).execute(holder.imageView );
  
-		return rowView;
+		return convertView;
 	}
+	
+    static class ViewHolder {
+        TextView titleView;
+        TextView authorView;
+        ImageView imageView;
+    }
 	
 	private class DownloadImage extends AsyncTask<Object, String, Bitmap> {
 		private ImageView imageView;
