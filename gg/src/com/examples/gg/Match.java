c10d5b89cd3700763be03b1b4e509944595e0ba0
whr0002 <my.temp2013@hotmail.com>
2013-07-21 22:01:37 -0600

    added match

---
+++ gg/src/com/examples/gg/Match.java
@@ -0,0 +1,65 @@
+package com.examples.gg;
+
+public class Match {
+	private String teamName1;
+	private String teamName2;
+	
+	private String teamIcon1;
+	private String teamIcon2;
+	
+	private String gosuLink;
+	
+	private String time;
+
+	
+	public String getTeamName1() {
+		return teamName1;
+	}
+
+	public void setTeamName1(String teamName1) {
+		this.teamName1 = teamName1;
+	}
+
+	public String getTeamName2() {
+		return teamName2;
+	}
+
+	public void setTeamName2(String teamName2) {
+		this.teamName2 = teamName2;
+	}
+
+	public String getTeamIcon1() {
+		return teamIcon1;
+	}
+
+	public void setTeamIcon1(String teamIcon1) {
+		this.teamIcon1 = teamIcon1;
+	}
+
+	public String getTeamIcon2() {
+		return teamIcon2;
+	}
+
+	public void setTeamIcon2(String teamIcon2) {
+		this.teamIcon2 = teamIcon2;
+	}
+
+	public String getGosuLink() {
+		return gosuLink;
+	}
+
+	public void setGosuLink(String gosuLink) {
+		this.gosuLink = gosuLink;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+	
+	
+}
