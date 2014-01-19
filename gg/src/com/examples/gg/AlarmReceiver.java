2aff1ec224a4352ed8037eb4d07a629211111917
Haoran <Haoran@Haoran-PC>
2014-01-19 00:03:30 -0700

    fixed notification

--- gg/src/com/examples/gg/AlarmReceiver.java
+++ gg/src/com/examples/gg/AlarmReceiver.java
@@ -1,51 +1,18 @@
 package com.examples.gg;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Vibrator;
+
+import com.commonsware.cwac.wakeful.WakefulIntentService;
 
 public class AlarmReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		NotificationManager mNM;
-
-		mNM = (NotificationManager) context
-				.getSystemService(context.NOTIFICATION_SERVICE);
-		// Set the icon, scrolling text and timestamp
-//		Log.i("debug", Integer.toString(intent.getIntExtra("rand", 1)));
-		Notification notification = new Notification(R.drawable.ic_launcher,
-				intent.getStringExtra("msg"), System.currentTimeMillis());
-		notification.flags = Notification.DEFAULT_LIGHTS
-				| Notification.FLAG_AUTO_CANCEL;
-		try {
-			Uri notificationURI = RingtoneManager
-					.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-			Ringtone r = RingtoneManager.getRingtone(context, notificationURI);
-			r.play();
-
-			Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-			// Vibrate for 500 milliseconds
-			v.vibrate(500);
-		} catch (Exception e) {
-		}
-
-		// The PendingIntent to launch our activity if the user selects this
-		// notification
-		PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
-				new Intent(context, SideMenuActivity.class), 0);
-		// Set the info for the views that show in the notification panel.
-		notification.setLatestEventInfo(context, "The game has started!",
-				intent.getStringExtra("msg"), contentIntent);
-		// Send the notification.
-		// We use a layout id because it is a unique number. We use it later to
-		// cancel.
-		mNM.notify(intent.getIntExtra("rand", 1), notification);
+//		Log.i("debug", "in receiver start");
+		Intent broadcast_intent = new Intent(context, NotificationService.class);
+        broadcast_intent.putExtra("msg", intent.getStringExtra("msg"));
+        broadcast_intent.putExtra("rand", intent.getIntExtra("rand", 1));   
+		WakefulIntentService.sendWakefulWork(context, broadcast_intent);
 	}
 }
--- gg/src/com/examples/gg/AlarmService.java
+++ gg/src/com/examples/gg/AlarmService.java
@@ -16,15 +16,14 @@ import android.content.Intent;
 	        Intent broadcast_intent = new Intent(context, AlarmReceiver.class);
 	        broadcast_intent.putExtra("msg", mTitle);
 	        broadcast_intent.putExtra("rand", mRand);   
-	        mAlarmSender = PendingIntent.getBroadcast(context, 0, broadcast_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+	        mAlarmSender = PendingIntent.getBroadcast(context, mRand, broadcast_intent, PendingIntent.FLAG_UPDATE_CURRENT);
 	    }
 
 	    public void startAlarm(long millseconds){
 	        //Set the alarm to 10 seconds from now
 //	        Calendar c = Calendar.getInstance();
-//	        c.add(Calendar.MILLISECOND, 1000);
-//	        long firstTime = c.getTimeInMillis() + millseconds;
-//	        Log.i("debug long", Long.toString(firstTime));
+//	        c.add(Calendar.SECOND, 10);
+//	        long firstTime = c.getTimeInMillis();
 	        // Schedule the alarm!
 	        AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
 	        am.set(AlarmManager.RTC_WAKEUP, millseconds, mAlarmSender);
